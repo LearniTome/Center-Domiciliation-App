acc8ca2895a2fcf7d4f48c61365b517404fbc6ab
Devopsabdel <devopsabdel@gmail.com>
2026-05-14 18:13:16 +0100

    Add Fichier column back to templates table, restore type label in Type de document

--- pages/generation.php
+++ pages/generation.php
@@ -327,6 +327,7 @@ $docxCount = $totalGenerated;
                             <tr>
                                 <th class="col-check"></th>
                                 <th>Type de document</th>
+                                <th>Fichier</th>
                                 <th>Champs</th>
                                 <th>Groupe</th>
                             </tr>
@@ -338,9 +339,9 @@ $docxCount = $totalGenerated;
                                         <td><input type="checkbox" name="templates[]" value="<?= e($tpl['path']) ?>" checked></td>
                                         <td>
                                             <span class="mdi mdi-file-word" style="color:var(--primary);margin-right:6px"></span>
-                                            <?php $tplLabel = $docTypesConfig[$tpl['doc_type']] ?? str_replace(['-', '_'], ' ', $tpl['doc_type']); ?>
-                                            <?= e(trim($tplLabel)) ?>
+                                            <?= e($docTypesConfig[$tpl['doc_type']] ?? $tpl['doc_type']) ?>
                                         </td>
+                                        <td><span class="help-text"><?= e(basename($tpl['path'])) ?></span></td>
                                         <td><?= count($tpl['variables']) ?></td>
                                         <td>
                                             <span class="mdi <?= $genTypeIcons[$gt] ?? 'mdi-file-document' ?>" style="color:var(--primary);margin-right:4px"></span>
