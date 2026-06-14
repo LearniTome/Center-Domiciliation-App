77ed0ebbb4796c5191872f93e57ecdaea88e1eb1
devopsabdel <devopsabdel@gmail.com>
2026-06-14 14:43:05 +0100

    rename entry point cession.php -> _main.php

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ $pageDir = [
 // Custom filenames for pages where the file != the page name
 $pageFile = [
     'cessions' => 'list',
-    'cession' => 'cession_steps/cession',
+    'cession' => 'cession_steps/_main',
     'cession_dossier' => 'detail',
 ];
 
