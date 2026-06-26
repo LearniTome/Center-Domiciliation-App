74beedc8da00f1f17e295f529f80ef6a1184e137
devopsabdel <devopsabdel@gmail.com>
2026-06-26 15:57:24 +0100

    fix: make e() accept string|int|float|null (fatal error when total_parts was int)

--- includes/fonctions.php
+++ includes/fonctions.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-function e(?string $value): string
+function e(string|int|float|null $value): string
 {
     return htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
 }
