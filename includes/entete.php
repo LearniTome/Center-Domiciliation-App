b7ff29d047f2cb9821fdc6c1ee6686317bbca89c
Devopsabdel <devopsabdel@gmail.com>
2026-08-25 10:12:08 +0100

    feat: add favicon using centre logo
    Favicon links to uploads/centre/logo.png in the browser tab.

--- includes/entete.php
+++ includes/entete.php
@@ -12,6 +12,7 @@ $noSidebar = in_array($page ?? '', ['connexion', 'deconnexion'], true);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= e($pageTitle) ?> | <?= e($config['app_name']) ?></title>
+    <link rel="icon" type="image/png" href="uploads/centre/logo.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap" rel="stylesheet">
